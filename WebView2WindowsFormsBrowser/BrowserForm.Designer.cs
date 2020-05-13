3facedd7c212880e9ca190ae8b94a7f1a8e15807
Jessica Chen <peiche@microsoft.com>
2020-05-12 22:08:05 -0700

    Initial commit for WebView2WindowsFormsBrowser

---
+++ WebView2WindowsFormsBrowser/BrowserForm.Designer.cs
@@ -0,0 +1,256 @@
+﻿// Copyright (C) Microsoft Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+using System;
+
+namespace WebView2WindowsFormsBrowser
+{
+    partial class BrowserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEvents = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoomToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.xToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.xToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.xToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.xToolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.webView2Control = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.SuspendLayout();
+            // 
+            // btnEvents
+            // 
+            this.btnEvents.Location = new System.Drawing.Point(1576, 48);
+            this.btnEvents.Margin = new System.Windows.Forms.Padding(6);
+            this.btnEvents.Name = "btnEvents";
+            this.btnEvents.Size = new System.Drawing.Size(150, 44);
+            this.btnEvents.TabIndex = 6;
+            this.btnEvents.Text = "Events";
+            this.btnEvents.UseVisualStyleBackColor = true;
+            this.btnEvents.Click += new System.EventHandler(this.btnEvents_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Enabled = false;
+            this.btnBack.Location = new System.Drawing.Point(24, 48);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(6);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(150, 44);
+            this.btnBack.TabIndex = 0;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnForward
+            // 
+            this.btnForward.Enabled = false;
+            this.btnForward.Location = new System.Drawing.Point(186, 48);
+            this.btnForward.Margin = new System.Windows.Forms.Padding(6);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(150, 44);
+            this.btnForward.TabIndex = 1;
+            this.btnForward.Text = "Forward";
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(348, 48);
+            this.btnRefresh.Margin = new System.Windows.Forms.Padding(6);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(150, 44);
+            this.btnRefresh.TabIndex = 2;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.BtnRefresh_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(510, 48);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(6);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(150, 44);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(1426, 48);
+            this.btnGo.Margin = new System.Windows.Forms.Padding(6);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(150, 44);
+            this.btnGo.TabIndex = 5;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.BtnGo_Click);
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(672, 48);
+            this.txtUrl.Margin = new System.Windows.Forms.Padding(6);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(738, 31);
+            this.txtUrl.TabIndex = 4;
+            this.txtUrl.Text = "https://www.bing.com/";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.viewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1580, 42);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(72, 38);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zoomToolStripMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(86, 38);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // zoomToolStripMenuItem
+            // 
+            this.zoomToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.xToolStripMenuItem,
+            this.xToolStripMenuItem1,
+            this.xToolStripMenuItem2,
+            this.xToolStripMenuItem3});
+            this.zoomToolStripMenuItem.Name = "zoomToolStripMenuItem";
+            this.zoomToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.zoomToolStripMenuItem.Text = "Zoom";
+            // 
+            // xToolStripMenuItem
+            // 
+            this.xToolStripMenuItem.Name = "xToolStripMenuItem";
+            this.xToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.xToolStripMenuItem.Text = "0.5x";
+            this.xToolStripMenuItem.Click += new System.EventHandler(this.xToolStripMenuItem05_Click);
+            // 
+            // xToolStripMenuItem1
+            // 
+            this.xToolStripMenuItem1.Name = "xToolStripMenuItem1";
+            this.xToolStripMenuItem1.Size = new System.Drawing.Size(359, 44);
+            this.xToolStripMenuItem1.Text = "1.0x";
+            this.xToolStripMenuItem1.Click += new System.EventHandler(this.xToolStripMenuItem1_Click);
+            // 
+            // xToolStripMenuItem2
+            // 
+            this.xToolStripMenuItem2.Name = "xToolStripMenuItem2";
+            this.xToolStripMenuItem2.Size = new System.Drawing.Size(359, 44);
+            this.xToolStripMenuItem2.Text = "2.0x";
+            this.xToolStripMenuItem2.Click += new System.EventHandler(this.xToolStripMenuItem2_Click);
+            // 
+            // xToolStripMenuItem3
+            // 
+            this.xToolStripMenuItem3.Name = "xToolStripMenuItem3";
+            this.xToolStripMenuItem3.Size = new System.Drawing.Size(359, 44);
+            this.xToolStripMenuItem3.Text = "Get ZoomFactor";
+            this.xToolStripMenuItem3.Click += new System.EventHandler(this.xToolStripMenuItem3_Click);
+            // 
+            // webView2Control
+            // 
+            this.webView2Control.Location = new System.Drawing.Point(0, 96);
+            this.webView2Control.Name = "webView2Control";
+            this.webView2Control.Size = new System.Drawing.Size(788, 410);
+            this.webView2Control.Source = new Uri("https://www.bing.com/");
+            this.webView2Control.TabIndex = 7;
+            this.webView2Control.Text = "webView2Control";
+            this.webView2Control.CoreWebView2Ready += WebView2Control_CoreWebView2Ready;
+            this.webView2Control.NavigationStarting += WebView2Control_NavigationStarting;
+            this.webView2Control.NavigationCompleted += WebView2Control_NavigationCompleted;
+            this.webView2Control.SourceChanged += WebView2Control_SourceChanged;
+            this.webView2Control.KeyDown += WebView2Control_KeyDown;
+            this.webView2Control.KeyUp += WebView2Control_KeyUp;
+            // 
+            // BrowserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1580, 865);
+            this.Controls.Add(this.webView2Control);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnEvents);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "BrowserForm";
+            this.Text = "BrowserForm";
+            this.Resize += new System.EventHandler(this.Form_Resize);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+        #endregion
+
+        private System.Windows.Forms.Button btnEvents;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtUrl;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView2Control;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zoomToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem xToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem xToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem xToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem xToolStripMenuItem3;
+    }
+}
+
---
+++ WebView2WindowsFormsBrowser/BrowserForm.cs
@@ -0,0 +1,148 @@
+﻿// Copyright (C) Microsoft Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+using System;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+
+namespace WebView2WindowsFormsBrowser
+{
+    public partial class BrowserForm : Form
+    {
+        public BrowserForm()
+        {
+            InitializeComponent();
+            HandleResize();
+        }
+
+        private void UpdateTitleWithEvent(string message)
+        {
+            string currentDocumentTitle = this.webView2Control?.CoreWebView2?.DocumentTitle ?? "Uninitialized";
+            this.Text = currentDocumentTitle + " (" + message + ")";
+        }
+
+        #region Event Handlers
+        private void WebView2Control_NavigationStarting(object sender, CoreWebView2NavigationStartingEventArgs e)
+        {
+            UpdateTitleWithEvent("NavigationStarting");
+        }
+
+        private void WebView2Control_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            UpdateTitleWithEvent("NavigationCompleted");
+        }
+
+        private void WebView2Control_SourceChanged(object sender, CoreWebView2SourceChangedEventArgs e)
+        {
+            txtUrl.Text = webView2Control.Source.AbsoluteUri;
+        }
+
+        private void WebView2Control_CoreWebView2Ready(object sender, EventArgs e)
+        {
+            this.webView2Control.CoreWebView2.SourceChanged += CoreWebView2_SourceChanged;
+            this.webView2Control.CoreWebView2.HistoryChanged += CoreWebView2_HistoryChanged;
+            this.webView2Control.CoreWebView2.DocumentTitleChanged += CoreWebView2_DocumentTitleChanged;
+            this.webView2Control.CoreWebView2.AddWebResourceRequestedFilter("*", CoreWebView2WebResourceContext.Image);
+            UpdateTitleWithEvent("CoreWebView2Ready");
+        }
+
+        private void WebView2Control_KeyUp(object sender, KeyEventArgs e)
+        {
+            UpdateTitleWithEvent($"AcceleratorKeyUp key={e.KeyCode}");
+        }
+
+        private void WebView2Control_KeyDown(object sender, KeyEventArgs e)
+        {
+            UpdateTitleWithEvent($"AcceleratorKeyDown key={e.KeyCode}");
+        }
+
+        private void CoreWebView2_HistoryChanged(object sender, object e)
+        {
+            // No explicit check for webView2Control initialization because the events can only start
+            // firing after the CoreWebView2 and its events exist for us to subscribe.
+            btnBack.Enabled = webView2Control.CoreWebView2.CanGoBack;
+            btnForward.Enabled = webView2Control.CoreWebView2.CanGoForward;
+            UpdateTitleWithEvent("HistoryChanged");
+        }
+
+        private void CoreWebView2_SourceChanged(object sender, CoreWebView2SourceChangedEventArgs e)
+        {
+            this.txtUrl.Text = this.webView2Control.Source.AbsoluteUri;
+            UpdateTitleWithEvent("SourceChanged");
+        }
+
+        private void CoreWebView2_DocumentTitleChanged(object sender, object e)
+        {
+            this.Text = this.webView2Control.CoreWebView2.DocumentTitle;
+            UpdateTitleWithEvent("DocumentTitleChanged");
+        }
+        #endregion
+
+        #region UI event handlers
+        private void BtnRefresh_Click(object sender, EventArgs e)
+        {
+            webView2Control.Reload();
+        }
+
+        private void BtnGo_Click(object sender, EventArgs e)
+        {
+            webView2Control.Source = new Uri(txtUrl.Text);
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            webView2Control.GoBack();
+        }
+
+        private void btnEvents_Click(object sender, EventArgs e)
+        {
+            (new EventMonitor(this.webView2Control)).Show(this);
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            webView2Control.GoForward();
+        }
+
+        private void Form_Resize(object sender, EventArgs e)
+        {
+            HandleResize();
+        }
+
+        private void xToolStripMenuItem05_Click(object sender, EventArgs e)
+        {
+            this.webView2Control.ZoomFactor = 0.5;
+        }
+
+        private void xToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.webView2Control.ZoomFactor = 1.0;
+        }
+
+        private void xToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            this.webView2Control.ZoomFactor = 2.0;
+        }
+
+        private void xToolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Zoom factor: {this.webView2Control.ZoomFactor}", "WebView Zoom factor");
+        }
+        #endregion
+
+        private void HandleResize()
+        {
+            // Resize the webview
+            webView2Control.Size = this.ClientSize - new System.Drawing.Size(webView2Control.Location);
+
+            // Move the Events button
+            btnEvents.Left = this.ClientSize.Width - btnEvents.Width;
+            // Move the Go button
+            btnGo.Left = this.btnEvents.Left - btnGo.Size.Width;
+
+            // Resize the URL textbox
+            txtUrl.Width = btnGo.Left - txtUrl.Left;
+        }
+    }
+}
