0d2a71b29c84b1a5b0598d97fa6130894197e61d
Jameson Li <jamesoli@microsoft.com>
2021-04-07 03:10:18 -0700

    Update Program.cs

--- SampleApps/WebView2WindowsFormsBrowser/Program.cs
+++ SampleApps/WebView2WindowsFormsBrowser/Program.cs
@@ -1,9 +1,10 @@
-﻿// Copyright (C) Microsoft Corporation. All rights reserved.
+// Copyright (C) Microsoft Corporation. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 using System;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 namespace WebView2WindowsFormsBrowser
 {
     static class Program
@@ -12,11 +13,19 @@ namespace WebView2WindowsFormsBrowser
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            if (args.Length > 0)
+            {
+                SetThreadDesktop(new IntPtr(int.Parse(args[0])));
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new BrowserForm());
         }
+
+        [DllImport("user32.dll")]
+        public static extern bool SetThreadDesktop(IntPtr hDesktop);
     }
 }
