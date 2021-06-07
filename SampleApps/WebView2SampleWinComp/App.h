a596c9fb1180b31a31545a75a7cdd3c9168096fb
Eve Le <thaol@microsoft.com>
2021-06-07 12:26:59 -0700

    Add wincomp sample app to WebView2Samples

---
+++ SampleApps/WebView2SampleWinComp/App.h
@@ -0,0 +1,9 @@
+// Copyright (C) Microsoft Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include "resource.h"
+extern HINSTANCE hInst;
+extern int nCmdShow;
