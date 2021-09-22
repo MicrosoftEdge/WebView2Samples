f17aa1491974734ba436e4c31f95c5384c0bdf0e
Lichen Liu <liulichen0603@163.com>
2021-09-22 17:15:39 +0800

    sdk release official-96.0.1023.0

--- SampleApps/WebView2WpfBrowser/MainWindow.xaml.cs
+++ SampleApps/WebView2WpfBrowser/MainWindow.xaml.cs
@@ -83,6 +83,7 @@ namespace WebView2WpfBrowser
         }
 
         List<CoreWebView2Frame> _webViewFrames = new List<CoreWebView2Frame>();
+        IReadOnlyList<CoreWebView2ProcessInfo> _processList;
 
 
         public MainWindow()
@@ -1058,6 +1059,7 @@ namespace WebView2WpfBrowser
                     {
                         WebViewEnvironment.BrowserProcessExited += Environment_BrowserProcessExited;
                         WebViewEnvironment.NewBrowserVersionAvailable += Environment_NewBrowserVersionAvailable;
+                        WebViewEnvironment.ProcessInfoChanged += WebView_ProcessInfoChanged;
                     }
                     catch (NotImplementedException)
                     {
