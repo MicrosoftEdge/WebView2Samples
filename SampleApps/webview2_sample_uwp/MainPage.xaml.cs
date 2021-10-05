dce29da9f39d44de6dd9bdaffe24b7d170a11576
David Shoemaker <dashoe@microsoft.com>
2021-10-04 17:59:03 -0700

    uwp sample

---
+++ SampleApps/webview2_sample_uwp/MainPage.xaml.cs
@@ -0,0 +1,104 @@
+﻿// Copyright (C) Microsoft Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+ #pragma warning disable CS8305 //'Microsoft.UI.Xaml.Controls.WebView2' is for evaluation purposes only and is subject to change or removal in future updates.
+
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.Web.WebView2.Core;
+using System;
+using System.Diagnostics;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace webview2_sample_uwp
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            AddressBar.Text = "https://developer.microsoft.com/en-us/microsoft-edge/webview2/";
+            WebView2.Source = new Uri(AddressBar.Text);
+
+            WebView2.NavigationCompleted += WebView2_NavigationCompleted;
+
+            StatusUpdate("Ready");
+        }
+
+        private void StatusUpdate(string message)
+        {
+            StatusBar.Text = message;
+            Debug.WriteLine(message);
+        }
+
+        private void WebView2_NavigationCompleted(WebView2 sender, CoreWebView2NavigationCompletedEventArgs args)
+        {
+            StatusUpdate("Navigation complete");
+        }
+
+        private bool TryCreateUri(String potentialUri, out Uri result)
+        {
+            StatusUpdate("TryCreateUri");
+
+            Uri uri;
+            if ((Uri.TryCreate(potentialUri, UriKind.Absolute, out uri) || Uri.TryCreate("http://" + potentialUri, UriKind.Absolute, out uri)) &&
+                (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps))
+            {
+                result = uri;
+                return true;
+            }
+            else
+            {
+                StatusUpdate("Unable to configure URI");
+                result = null;
+                return false;
+            }
+        }
+
+        private void TryNavigate()
+        {
+            StatusUpdate("TryNavigate");
+
+            Uri destinationUri;
+            if (TryCreateUri(AddressBar.Text, out destinationUri))
+            {
+                WebView2.Source = destinationUri;
+            }
+            else
+            {
+                StatusUpdate("URI couldn't be figured out use it as a bing search term");
+
+                String bingString = "https://www.bing.com/search?q=" + Uri.EscapeUriString(AddressBar.Text);
+                if (TryCreateUri(bingString, out destinationUri))
+                {
+                    AddressBar.Text = destinationUri.AbsoluteUri;
+                    WebView2.Source = destinationUri;
+                }
+                else
+                {
+                    StatusUpdate("URI couldn't be configured as bing search term, giving up");
+                }
+            }
+        }
+
+        private void Go_OnClick(object sender, RoutedEventArgs e)
+        {
+            StatusUpdate("Go_OnClick: " + AddressBar.Text);
+
+            TryNavigate();
+        }
+
+        private void AddressBar_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                StatusUpdate("AddressBar_KeyDown [Enter]: " + AddressBar.Text);
+
+                e.Handled = true;
+                TryNavigate();
+            }
+        }
+    }
+}
---
+++ SampleApps/webview2_sample_uwp/properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("webview2_sample_uwp")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyTitleAttribute("webview2_sample_uwp")]
+[assembly: AssemblyDescriptionAttribute("A WebView2 WinUI2 UWP test application")]
+[assembly: AssemblyCompanyAttribute("Microsoft")]
+[assembly: AssemblyCopyrightAttribute("Copyright © Microsoft Inc. 2021")]
