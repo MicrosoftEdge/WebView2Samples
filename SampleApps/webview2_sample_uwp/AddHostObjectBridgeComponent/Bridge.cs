36d277975aaf739ff1aced1cca5fe0a8e8a10bc0
Piet Schouten <Piet.Schouten@microsoft.com>
2022-11-21 15:45:42 -0800

    Update UWP sample app.

---
+++ SampleApps/webview2_sample_uwp/AddHostObjectBridgeComponent/Bridge.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Windows.Foundation;
+
+namespace AddHostObjectBridgeComponent
+{
+    public sealed class Bridge
+    {
+        private List<string> _items = new List<string>();
+        public event TypedEventHandler<Bridge, IList<string>> ItemsChangedEvent;
+
+        public Bridge() { }
+
+        public string StringProperty { get; set; } = "example";
+
+        public void ClearItems()
+        {
+            _items.Clear();
+            PostItemsChangedEvent();
+        }
+
+        public void AppendToItems(string item)
+        {
+            _items.Add(item);
+            PostItemsChangedEvent();
+        }
+
+        private void PostItemsChangedEvent()
+        {
+            if (ItemsChangedEvent != null)
+            {
+                ItemsChangedEvent(this, _items);
+            }
+        }
+    }
+}
+
---
+++ SampleApps/webview2_sample_uwp/WinRTAdapter/pch.h
@@ -0,0 +1,4 @@
+﻿#pragma once
+#include <unknwn.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Foundation.h>
