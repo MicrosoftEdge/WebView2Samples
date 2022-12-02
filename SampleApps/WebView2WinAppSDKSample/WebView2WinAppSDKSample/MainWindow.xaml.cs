7f2fb288add708c4c3e85accd953fa0816e23e62
Jason Stephen <jasteph@microsoft.com>
2022-12-01 16:36:28 -0800

    Adding WebView2

--- SampleApps/WebView2WinAppSDKSample/WebView2WinAppSDKSample/MainWindow.xaml.cs
+++ SampleApps/WebView2WinAppSDKSample/WebView2WinAppSDKSample/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
+
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
@@ -33,7 +34,7 @@ namespace WebView2WinAppSDKSample
 
         private void myButton_Click(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
+            //myButton.Content = "Clicked";
         }
     }
 }
